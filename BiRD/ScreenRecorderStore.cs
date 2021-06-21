cc683d61ccc92544436d0db0541a63329aba299e
Kiwin Andersen <kikoro@live.dk>
2021-06-21 14:03:08 +0200

    extract ScreenRecorder from RecorderWindow

---
+++ BiRD/ScreenRecorderStore.cs
@@ -0,0 +1,16 @@
+﻿namespace BiRD
+{
+    public static class ScreenRecorderStore
+    {
+        private static ScreenRecorder _screenRecorder;
+        public static ScreenRecorder
+            GetRecorder()
+        {
+            if (_screenRecorder == null)
+            {
+                _screenRecorder = new ScreenRecorder();
+            }
+            return _screenRecorder;
+        }
+    }
+}
